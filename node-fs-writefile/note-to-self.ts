00e725eb480fd8a53849e9e993933aa0b597613c
Andrew Roide <andrew.roide@hotmail.com>
2024-02-09 04:59:25 +0000

    updated code to reflect comments on PR

--- node-fs-writefile/note-to-self.ts
+++ node-fs-writefile/note-to-self.ts
@@ -1,14 +1,10 @@
 import { writeFile } from 'node:fs/promises';
 
-async function writeNote() {
-  try {
-    const filePath = 'note.txt';
-    const data = process.argv[2];
-    await writeFile(filePath, data);
-    console.log(filePath);
-  } catch (error) {
-    console.log('Error writing file', error);
-  }
+try {
+  const filePath = 'note.txt';
+  const data = process.argv[2];
+  await writeFile(filePath, data);
+  console.log(filePath);
+} catch (error) {
+  console.log('Error writing file', error);
 }
-
-writeNote();
--- node-fs-writefile/write-read.ts
+++ node-fs-writefile/write-read.ts
@@ -1,15 +1,10 @@
 import { writeFile } from 'node:fs/promises';
 
-async function randomNumber() {
-  try {
-    const filePath = 'randomNum.txt';
-    const data = Math.floor(Math.random() * 11);
-    await writeFile(filePath, data.toString());
-    console.log(filePath);
-
-  } catch (error) {
-    console.log('Error writing file', error);
-  }
+try {
+  const filePath = 'randomNum.txt';
+  const data = Math.floor(Math.random() * 11);
+  await writeFile(filePath, data.toString());
+  console.log(filePath);
+} catch (error) {
+  console.log('Error writing file', error);
 }
-
-randomNumber();
