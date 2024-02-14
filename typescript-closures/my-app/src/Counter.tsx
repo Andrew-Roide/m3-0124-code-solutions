a536157ff9c29a05232da2bb65ff82899eb3b58b
Andrew Roide <andrew.roide@hotmail.com>
2024-02-14 05:10:33 +0000

    Completed typescript closures

---
+++ typescript-closures/my-app/src/Counter.tsx
@@ -0,0 +1,12 @@
+import { useCallback, useEffect, useState } from 'react';
+
+export function Counter() {
+  const [counter, setCounter] = useState(0);
+  const getData = useCallback(() => ({ foo: 'bar' }), []);
+  useEffect(() => {
+    getData();
+    setCounter((prev) => prev + 1);
+  }, [getData]);
+
+  return <div>Fetching {counter} times</div>;
+}
